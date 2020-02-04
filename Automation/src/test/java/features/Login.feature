d475b55104a47d947d6f56118b8f5b7b63368e15
E004568 <E004568@CTLINDT-0696.cignitiglobal.com>
2020-02-04 18:51:45 +0530

    Added the code for new features

--- Automation/src/test/java/features/Login.feature
+++ Automation/src/test/java/features/Login.feature
@@ -2,6 +2,37 @@ Feature: Application Login
 
 Scenario: Dashboard screen after Login
 Given User is on the Banks Landing page
-When User enters the valid Username and Password
+When User enters the valid Username "Harikrishna" and Password "Revolution"
 Then Customer dashboard is displayed
-And Customers info cards are displayed
+And Customers info cards are displayed is "true"
+
+
+Scenario: Dashboard screen after Login
+Given User is on the Banks Landing page
+When User enters the valid Username "Joshnika" and Password "Evolution"
+Then Customer dashboard is displayed
+And Customers info cards are displayed is "false"
+
+
+Scenario: Dashboard screen after Login
+Given User is on the Banks Landing page
+When User enters the below details
+| Harikrishna | Revolution | Fresh Living Appartments | Madhapur | Hyderabad |
+Then Customer dashboard is displayed
+And Customers info cards are displayed is "false"
+
+
+Scenario: Login Multiple Times
+Given User is on the Banks Landing page
+When User enters the valid <Username> and <Password>
+Then Customer dashboard is displayed
+And Customers info cards are displayed is "true"
+
+Examples:
+|Username | Password |
+|Hari     | Krishna  |
+|Joshnika | Swarna   |
+|Pushpa   | Latha    |
+|Mounica  | Nareddy  |
+|Ved      | Rishikesh|
+
--- Automation/src/test/java/stepDefinitions/StepDefinition.java
+++ Automation/src/test/java/stepDefinitions/StepDefinition.java
@@ -1,7 +1,10 @@
 package stepDefinitions;
 
+import java.util.List;
+
 import org.junit.runner.RunWith;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -17,10 +20,18 @@ public class StepDefinition {
 
 	}
 
-	@When("^User enters the valid Username and Password$")
-	public void user_enters_the_valid_username_and_password() throws Throwable {
-		System.out.println("User entered the valid credentials and clicked Submit button");
+	/*
+	  @When("^User enters the valid Username and Password$") public void
+	 user_enters_the_valid_username_and_password() throws Throwable { System.out.
+	 println("User entered the valid credentials and clicked Submit button");
+	 }
+	 */
 
+	@When("^User enters the valid Username \"([^\"]*)\" and Password \"([^\"]*)\"$")
+	public void user_enters_the_valid_Username_and_Password(String arg1, String arg2) throws Throwable {
+		System.out.println(arg1);
+		System.out.println(arg2);
+		
 	}
 
 	@Then("^Customer dashboard is displayed$")
@@ -29,10 +40,29 @@ public class StepDefinition {
 
 	}
 
-	@And("^Customers info cards are displayed$")
+	/*@And("^Customers info cards are displayed$")
 	public void customers_info_cards_are_displayed() throws Throwable {
 		System.out.println("Customer info cards are displayed");
 
+	}*/
+	
+	@Then("^Customers info cards are displayed is \"([^\"]*)\"$")
+	public void customers_info_cards_are_displayed_is(String arg1) throws Throwable {
+		System.out.println(arg1);
+	   
 	}
+	
+	
+	 @When("^User enters the below details$")
+	    public void user_enters_the_below_details(DataTable data) throws Throwable {
+		 List<List<String>> obj = data.raw();
+		 System.out.println(obj.get(0).get(0));
+		 System.out.println(obj.get(0).get(1));
+		 System.out.println(obj.get(0).get(2));
+		 System.out.println(obj.get(0).get(3));
+		 System.out.println(obj.get(0).get(4));
+		 
+	      
+	    }
 
 }
