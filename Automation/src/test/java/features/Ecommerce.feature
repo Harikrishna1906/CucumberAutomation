c338e27b75e87c1092aa036609e6986eca78eb36
E004568 <E004568@CTLINDT-0696.cignitiglobal.com>
2020-02-11 18:56:42 +0530

    Injecting selenium code

--- Automation/src/test/java/features/Ecommerce.feature
+++ Automation/src/test/java/features/Ecommerce.feature
@@ -3,4 +3,4 @@ Feature: Ecommerce search
 Scenario: Search Results
 Given User is on the Landing page
 When user searches for the "Cucumber" vegetable
-Then "Cucumber" results should be displayed
+Then "cucumber" results should be displayed
--- Automation/src/test/java/stepDefinitions/EcommerceStepDefinition.java
+++ Automation/src/test/java/stepDefinitions/EcommerceStepDefinition.java
@@ -1,7 +1,48 @@
 package stepDefinitions;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class EcommerceStepDefinition {
 	
-	
+	WebDriver driver;
+
+	@Given("^User is on the Landing page$")
+	public void user_is_on_the_landing_page() throws Throwable {
+		System.setProperty("webdriver.chrome.driver", "Resource\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("Https://rahulshettyacademy.com/seleniumPractise");
+		String webpageName = driver.getWindowHandle().toString();
+		System.out.println(webpageName);
+
+	}
+
+	@When("^user searches for the \"([^\"]*)\" vegetable$")
+	public void user_searches_for_the_something_vegetable(String strArg1) throws Throwable {
+		
+		WebElement ele = driver.findElement(By.xpath("//input[@type='search']"));
+		ele.clear();
+		ele.sendKeys(strArg1);
+
+	}
+
+	@Then("^\"([^\"]*)\" results should be displayed$")
+	public void something_results_should_be_displayed(String strArg1) throws Throwable {
+		WebElement vegName = driver.findElement(By.xpath("//h4[@class='product-name'] "));
+		String vegetable = vegName.getText().toString();
+		Assert.assertEquals(strArg1, vegetable);
+		
+		
+			
+
+	}
 
 }
